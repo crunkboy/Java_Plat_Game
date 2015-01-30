1fe64244042475c9b35566ecb5797aafe3e369d8
crunkboy <etudiant397@gmail.com>
2015-01-30 19:12:59 +0100

    ok

--- JavaPlatGame/src/fr/uha/ensisa/abalone_game/Controller/Controle.java
+++ JavaPlatGame/src/fr/uha/ensisa/abalone_game/Controller/Controle.java
@@ -1,5 +1,31 @@
 package fr.uha.ensisa.abalone_game.Controller;
 
-public class Controle {
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import fr.uha.ensisa.abalone_game.View.*;
+import fr.uha.ensisa.abalone_game.Model.Boule;
+import fr.uha.ensisa.abalone_game.Model.Joueur;
+import fr.uha.ensisa.abalone_game.Model.Partie;
+import fr.uha.ensisa.abalone_game.Model.Trou;
+
+public class Controle implements KeyListener,MouseListener,ActionListener {
 
 }
